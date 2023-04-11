b9deccb2a405c5ed1f5b3d0bbb2512c3827d5817
ShivaPandey02 <shiva.pandey@crowdbotics.com>
2023-04-11 19:29:43 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "broken-lab-39355",
-  url: "https://broken-lab-39355.botics.co",
-  api: "https://broken-lab-39355.botics.co/api/v1"
-}
+  "name": "broken-lab-39355",
+  "url": "https://broken-lab-39355.botics.co",
+  "api": "https://broken-lab-39355.botics.co/api/v1",
+  "initialRoute": "Api"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
