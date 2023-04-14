8a3d6826b15a2dc7972b2d0906f1061df818c71f
Nenad Jovanovic <nenad.jovanovic@crowdbotics.com>
2023-04-14 07:17:36 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/brokenlabAPI/api.js
+++ store/brokenlabAPI/api.js
@@ -1,112 +1,132 @@
-import axios from "axios"
+import axios from "axios";
 const brokenlabAPI = axios.create({
   baseURL: "https://broken-lab-39355-prod.herokuapp.com",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_v1_login_create(payload) {
-  return brokenlabAPI.post(`/api/v1/login/`, payload.data)
+  return brokenlabAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return brokenlabAPI.post(`/api/v1/signup/`, payload.data)
+  return brokenlabAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return brokenlabAPI.get(`/rest-auth/user/`)
+  return brokenlabAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return brokenlabAPI.put(`/rest-auth/user/`, payload.data)
+  return brokenlabAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return brokenlabAPI.patch(`/rest-auth/user/`, payload.data)
+  return brokenlabAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 function api_docs_schema_retrieve(payload) {
   return brokenlabAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function rest_auth_login_create(payload) {
-  return brokenlabAPI.post(`/rest-auth/login/`, payload.data)
+  return brokenlabAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return brokenlabAPI.get(`/rest-auth/logout/`)
+  return brokenlabAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return brokenlabAPI.post(`/rest-auth/logout/`)
+  return brokenlabAPI.post(`/rest-auth/logout/`);
 }
+
 function modules_privacy_policy_list(payload) {
-  return brokenlabAPI.get(`/modules/privacy-policy/`)
+  return brokenlabAPI.get(`/modules/privacy-policy/`);
 }
+
 function modules_privacy_policy_create(payload) {
-  return brokenlabAPI.post(`/modules/privacy-policy/`, payload.data)
+  return brokenlabAPI.post(`/modules/privacy-policy/`, payload.data);
 }
+
 function rest_auth_registration_create(payload) {
-  return brokenlabAPI.post(`/rest-auth/registration/`, payload.data)
+  return brokenlabAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return brokenlabAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return brokenlabAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_change_create(payload) {
-  return brokenlabAPI.post(`/rest-auth/password/change/`, payload.data)
+  return brokenlabAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function modules_privacy_policy_retrieve(payload) {
-  return brokenlabAPI.get(`/modules/privacy-policy/${payload.id}/`)
+  return brokenlabAPI.get(`/modules/privacy-policy/${payload.id}/`);
 }
+
 function modules_privacy_policy_update(payload) {
-  return brokenlabAPI.put(
-    `/modules/privacy-policy/${payload.id}/`,
-    payload.data
-  )
+  return brokenlabAPI.put(`/modules/privacy-policy/${payload.id}/`, payload.data);
 }
+
 function modules_privacy_policy_partial_update(payload) {
-  return brokenlabAPI.patch(
-    `/modules/privacy-policy/${payload.id}/`,
-    payload.data
-  )
+  return brokenlabAPI.patch(`/modules/privacy-policy/${payload.id}/`, payload.data);
 }
+
 function modules_privacy_policy_destroy(payload) {
-  return brokenlabAPI.delete(`/modules/privacy-policy/${payload.id}/`)
+  return brokenlabAPI.delete(`/modules/privacy-policy/${payload.id}/`);
 }
+
 function modules_terms_and_conditions_list(payload) {
-  return brokenlabAPI.get(`/modules/terms-and-conditions/`)
+  return brokenlabAPI.get(`/modules/terms-and-conditions/`);
 }
+
 function modules_terms_and_conditions_create(payload) {
-  return brokenlabAPI.post(`/modules/terms-and-conditions/`, payload.data)
+  return brokenlabAPI.post(`/modules/terms-and-conditions/`, payload.data);
 }
+
 function modules_payments_payment_sheet_create(payload) {
-  return brokenlabAPI.post(`/modules/payments/payment_sheet/`)
+  return brokenlabAPI.post(`/modules/payments/payment_sheet/`);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return brokenlabAPI.post(`/rest-auth/password/reset/confirm/`, payload.data)
+  return brokenlabAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function modules_terms_and_conditions_retrieve(payload) {
-  return brokenlabAPI.get(`/modules/terms-and-conditions/${payload.id}/`)
+  return brokenlabAPI.get(`/modules/terms-and-conditions/${payload.id}/`);
 }
+
 function modules_terms_and_conditions_update(payload) {
-  return brokenlabAPI.put(
-    `/modules/terms-and-conditions/${payload.id}/`,
-    payload.data
-  )
+  return brokenlabAPI.put(`/modules/terms-and-conditions/${payload.id}/`, payload.data);
 }
+
 function modules_terms_and_conditions_partial_update(payload) {
-  return brokenlabAPI.patch(
-    `/modules/terms-and-conditions/${payload.id}/`,
-    payload.data
-  )
+  return brokenlabAPI.patch(`/modules/terms-and-conditions/${payload.id}/`, payload.data);
 }
+
 function modules_terms_and_conditions_destroy(payload) {
-  return brokenlabAPI.delete(`/modules/terms-and-conditions/${payload.id}/`)
+  return brokenlabAPI.delete(`/modules/terms-and-conditions/${payload.id}/`);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return brokenlabAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
+  return brokenlabAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 function modules_payments_get_payments_history_retrieve(payload) {
-  return brokenlabAPI.get(`/modules/payments/get_payments_history/`)
+  return brokenlabAPI.get(`/modules/payments/get_payments_history/`);
 }
+
 function modules_payments_get_payments_methods_retrieve(payload) {
-  return brokenlabAPI.get(`/modules/payments/get_payments_methods/`)
+  return brokenlabAPI.get(`/modules/payments/get_payments_methods/`);
 }
+
 export const apiService = {
   api_v1_login_create,
   api_v1_signup_create,
@@ -137,4 +157,4 @@ export const apiService = {
   rest_auth_registration_verify_email_create,
   modules_payments_get_payments_history_retrieve,
   modules_payments_get_payments_methods_retrieve
-}
+};
